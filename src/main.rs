74a6ba252d38f81a89a4a75362a38140f199dfa6
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-11-08 16:32:06 +0100

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,38 @@
 #[allow(unused_imports)]
 use tracing::*;
 
+mod io;
+
 fn main() {
     // Initialize tracing (Logger)
     tracing_subscriber::fmt::init();
 
-    println!("Hello World!");
+    let input_contents = io::read_file("./input.txt").unwrap();
+
+    let mut sum: f64 = 0.0;
+    let mut product: f64 = 1.0;
+    let mut difference: f64 = 0.0;
+    let mut quotient: f64 = 0.0;
+
+    input_contents.lines().enumerate().for_each(|(idx, line)| {
+        let num: f64 = line.parse().unwrap();
+        sum += num;
+        product *= num;
+        if idx == 0 {
+            difference = num;
+            quotient = num;
+        } else {
+            difference -= num;
+            quotient /= num;
+        }
+    });
+
+    let mut output_contents: String = String::new();
+
+    output_contents.push_str(&format!("Summe: {}\n", sum));
+    output_contents.push_str(&format!("Produkt: {}\n", product));
+    output_contents.push_str(&format!("Differenz: {}\n", difference));
+    output_contents.push_str(&format!("Quotient: {}\n", quotient));
+
+    io::write_file("./output.txt", &output_contents).unwrap();
 }
