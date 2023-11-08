685d7f18f2e7ac063bab939ff4349ef4fb51ba6d
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-11-08 16:46:40 +0100

    added debug messages

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ fn main() {
     // Initialize tracing (Logger)
     tracing_subscriber::fmt::init();
 
+    debug!("reading file");
     let input_contents = io::read_file("./input.txt").unwrap();
 
+    debug!("Preparing result variables");
     let mut sum: f64 = 0.0;
     let mut product: f64 = 1.0;
     let mut difference: f64 = 0.0;
     let mut quotient: f64 = 0.0;
 
     input_contents.lines().enumerate().for_each(|(idx, line)| {
+        debug!("Parsing line {}", idx + 1);
         let num: f64 = line.parse().unwrap();
+        debug!("Calculating result");
         sum += num;
         product *= num;
         if idx == 0 {
@@ -28,6 +32,8 @@ fn main() {
         }
     });
 
+    debug!("Generating output");
+
     let mut output_contents: String = String::new();
 
     output_contents.push_str(&format!("Summe: {}\n", sum));
@@ -35,5 +41,6 @@ fn main() {
     output_contents.push_str(&format!("Differenz: {}\n", difference));
     output_contents.push_str(&format!("Quotient: {}\n", quotient));
 
+    debug!("Writing output to file");
     io::write_file("./output.txt", &output_contents).unwrap();
 }
