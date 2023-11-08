91deff12b9daa9ee4e8a4abb48fbfee92e05d682
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-11-08 16:36:07 +0100

    added ignore flags for io functions

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,5 @@
 use std::{error::Error, io::Write};
+#[allow(unused)]
 pub fn input(prompt: &str) -> Result<String, Box<dyn Error>> {
     print!("{}", prompt);
     match std::io::stdout().flush() {
@@ -13,12 +14,14 @@ pub fn input(prompt: &str) -> Result<String, Box<dyn Error>> {
     Ok(input)
 }
 
+#[allow(unused)]
 pub fn read_file(filename: &str) -> Result<String, Box<dyn Error>> {
     let contents: String = std::fs::read_to_string(filename)?;
     Ok(contents)
 }
 
 use std::path::Path;
+#[allow(unused)]
 pub fn write_file(filename: &str, contents: &str) -> Result<(), Box<dyn Error>> {
     let mut file = std::fs::File::create(Path::new(filename))?;
     file.write_all(contents.as_bytes())?;
