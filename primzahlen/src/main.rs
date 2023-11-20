1a2e87b7301705f42c0c65cd3eda2f07a2eb8e33
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-11-20 20:57:20 +0100

    Refactor input parsing and add debug logging for prime number testing

--- primzahlen/src/main.rs
+++ primzahlen/src/main.rs
@@ -9,8 +9,9 @@ fn main() {
     tracing_subscriber::fmt::init();
 
     let eingabe = iolib::input("Zahl: ").unwrap();
-
     let eingabe = eingabe.parse::<i32>().unwrap();
+
+    // let eingabe = 100_000_007;
 
     if is_prime(eingabe) {
         println!("{} ist eine Primzahl!", eingabe);
@@ -24,6 +25,7 @@ fn is_prime(number: i32) -> bool {
         return false;
     }
     for i in 2..number {
+        debug!("Testing for {}", i);
         if number % i == 0 {
             return false;
         }
