7ffbc8beb35242c9ff39f417432d53f6e01da561
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2024-01-17 19:15:47 +0000

    Added basic reading functionality

--- api-kram/src/main.rs
+++ api-kram/src/main.rs
@@ -2,9 +2,9 @@
 #[allow(unused_imports)]
 use tracing::*;
 
-// // Import astralib (use `cargo update` to download the library)
-// #[allow(unused_imports)]
-// extern crate astralib;
+// Import astralib (use `cargo update` to download the library)
+#[allow(unused_imports)]
+extern crate astralib;
 
 use serde_json::Value;
 
@@ -12,7 +12,7 @@ use serde_json::Value;
 async fn main() {
     // Initialize tracing (Logger)
     tracing_subscriber::fmt::init();
-    
+
     debug!("Sending Request to API.");
     let body = reqwest::get("https://www.tagesschau.de/api2/news/?regions=9&resort=inland")
         .await
@@ -24,16 +24,70 @@ async fn main() {
     debug!("Parsing Response.");
     let data: Value = serde_json::from_str(&body).expect("The data should be in JSON.");
 
+    display_news_preview(&data);
+
+    loop {
+        let choice = astralib::io::input("Welchen Artikel möchten Sie lesen?: ")
+            .expect("choice should be read.");
+
+        if let Ok(choice) = choice.parse::<usize>() {
+            if &choice
+                <= &data["news"]
+                    .as_array()
+                    .expect("News should be available.")
+                    .len()
+            {
+                display_news(&data, choice);
+                
+                break;
+            } else {
+                error!("User selected article that is out of range!");
+                continue;
+            }
+        } else {
+            error!("Failed to parse into u32");
+        }
+    }
+}
+
+fn display_news_preview(data: &Value) {
     debug!("Extracting news.");
-    let news = data["news"].as_array().expect("News should be available.");
+    let news = &data["news"].as_array().expect("News should be available.");
 
     debug!("Displaying article previews.");
-    for article in news {
+    for (idx, article) in news.iter().enumerate() {
         trace!("Printing Article preview.");
         println!(
-            "{} - {}\n",
-            article["title"].as_str().expect("The title should be available as a str."),
-            article["firstSentence"].as_str().expect("The first sentence should be available as a str.")
+            "Artikel {:02}:\n\t {} - {}\n",
+            (idx + 1),
+            article["title"]
+                .as_str()
+                .expect("The title should be available as a str."),
+            article["firstSentence"]
+                .as_str()
+                .expect("The first sentence should be available as a str.")
         );
     }
+}
+
+fn display_news(data: &Value, choice: usize) {
+    // TODO: Implement reading inside of console using the details file.
+    let url = &data["news"][choice - 1]["detailsweb"]
+        .as_str()
+        .expect("The URL should be available.");
+    match open::that(url) {
+        Ok(_) => {
+           
+        }
+        Err(error) => {
+            error!("Could not open article: {:#?}", error);
+            println!(
+                "Could not open article automatically. Please visit\n{}",
+                url
+            )
+            
+        }
+    }
+
+    
 }
