6ce994631a9b5ad062bb77165635d0cdf5ff479c
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2024-01-17 18:28:10 +0000

    added debug statements

--- api-kram/src/main.rs
+++ api-kram/src/main.rs
@@ -10,6 +10,10 @@ use serde_json::Value;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    // Initialize tracing (Logger)
+    tracing_subscriber::fmt::init();
+    
+    debug!("Sending Request to API.");
     let body = reqwest::get("https://www.tagesschau.de/api2/news/?regions=9&resort=inland")
         .await
         .expect("The Request should return a Response.")
@@ -17,10 +21,15 @@ async fn main() {
         .await
         .expect("The Result should contain text.");
 
+    debug!("Parsing Response.");
     let data: Value = serde_json::from_str(&body).expect("The data should be in JSON.");
 
-    let news = data["news"].as_array().expect("News should be available");
+    debug!("Extracting news.");
+    let news = data["news"].as_array().expect("News should be available.");
+
+    debug!("Displaying article previews.");
     for article in news {
+        trace!("Printing Article preview.");
         println!(
             "{} - {}\n",
             article["title"].as_str().expect("The title should be available as a str."),
